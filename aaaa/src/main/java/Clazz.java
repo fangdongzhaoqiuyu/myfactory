d7bcace289a43c8d2e278f91e2fe00eea9ce65fe
fangdongzhaoqiuyu <1273010886@qq.com>
2018-08-15 14:57:00 +0800

    第四次提交类中增加了两个类

---
+++ aaaa/src/main/java/Clazz.java
@@ -0,0 +1,34 @@
+public class Clazz {
+
+    private int id;
+    private String cname;
+
+    public Clazz(int id, String cname) {
+        this.id = id;
+        this.cname = cname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+
+    @Override
+    public String toString() {
+        return "Clazz{" +
+                "id=" + id +
+                ", cname='" + cname + '\'' +
+                '}';
+    }
+}
---
+++ aaaa/src/main/java/User.java
@@ -0,0 +1,45 @@
+public class User {
+
+    private int id;
+    private String userName;
+    private String address;
+
+    public User(int id, String userName, String address) {
+        this.id = id;
+        this.userName = userName;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
