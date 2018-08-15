564f778d9e60040ebfe375d10e2f7aa420fff0b5
fangdongzhaoqiuyu <1273010886@qq.com>
2018-08-15 14:50:47 +0800

    第三次提交类中增加了一个map集合

--- aaaa/src/main/java/MyTest.java
+++ aaaa/src/main/java/MyTest.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyTest {
 
     public String queryList(){
+
+        Map<String, Object> map = new HashMap<String, Object>();
 
         return "";
     }
